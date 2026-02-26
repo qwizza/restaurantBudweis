6f63e1b87244143969e995d59fbd17d2d6d04d4a
qwizza <meowmeowpiupiu@mail.ru>
2026-02-26 08:03:48 +0300

    Добавлена таблица  DishsGroupDishs и незаконена работа со страницами

--- restaurantBudweis/Pages/Index.cshtml.cs
+++ restaurantBudweis/Pages/Index.cshtml.cs
@@ -7,16 +7,17 @@ namespace restaurantBudweis.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        private readonly ApplicationDbContext _context; 
 
         public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context; 
         }
 
         public void OnGet()
         {
-
+            var Dishs = _context.Dishs.ToList();
         }
     }
 }
